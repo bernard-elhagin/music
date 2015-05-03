ab901db6ff0cc4ad9a9471b2b66aa243a7f3c596
Bernard El-Hagin <bernard.elhagin@gmail.com>
2015-05-03 22:39:54 +0200

    Zadanie 8, cz. 2.

--- pl.atena.aj.be.zad8/src/main/java/pl/atena/aj/be/zad8/Main.java
+++ pl.atena.aj.be.zad8/src/main/java/pl/atena/aj/be/zad8/Main.java
@@ -5,15 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		Zad8 z8 = new Zad8();
 		
-		Double[][] d = {
-				{ 99.44, 33.899 }, { 9484.0033, 43837.09 } };
+		String[][] tablica = z8.construct2DTable(args, args.length);
 		
-		z8.display2DArray(d);
-
-		String[][] s = {
-				{ "ddd", "dddddd" }, { "eee", "eeeeee" } };
-
-		z8.display2DArray(s);
+		z8.display2DTable(tablica);
 	}
+
 
 }
