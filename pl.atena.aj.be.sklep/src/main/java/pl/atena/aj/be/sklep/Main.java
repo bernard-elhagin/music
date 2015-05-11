a1e8c02457b0067b47c43df7aeb88baafdc2331a
Bernard El-Hagin <bernard.elhagin@gmail.com>
2015-05-11 21:25:23 +0200

    Zadanie 24, cz. 1.

--- pl.atena.aj.be.sklep/src/main/java/pl/atena/aj/be/sklep/Main.java
+++ pl.atena.aj.be.sklep/src/main/java/pl/atena/aj/be/sklep/Main.java
@@ -2,14 +2,37 @@ package pl.atena.aj.be.sklep;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Shop shop = new Shop();
-		
-		Article rower = new Article(3, "Rower", 799.00, "Fajny rower");
-		
-		Shop.Order order = shop.new Order(5, rower);
-		order.accept();
-		
-		System.out.println(shop.toString());
-	}
+    public static void main(String[] args) {
+
+    	
+    	/* 
+    	 * Bez obslugi wyjatkow.
+    	 */
+    	
+//    	System.out.println("Dzielenie przez zero: " + 1 / 0);
+//    	
+//    	int[] tab = {9, 298};
+//    	
+//    	System.out.println("Pierwszy element: " + tab[0]);
+//    	System.out.println("Drugi element: " + tab[1]);
+//    	System.out.println("Trzeci element: " + tab[2]);
+    	
+    	/*
+    	 * Z obsluga wyjatklw.
+    	 */
+    	
+    	try {
+    		int x = 1 / 0;
+    	} catch (ArithmeticException e) {
+    		System.out.println(e.getMessage());
+    	}
+    	
+    	int[] tab = {8, 36};
+    	
+    	try {
+    		int x = tab[2];
+    	} catch (IndexOutOfBoundsException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
 }
