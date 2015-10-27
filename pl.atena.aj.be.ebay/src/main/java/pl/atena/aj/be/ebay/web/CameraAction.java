a0abe1f68eff9f92e013d8f39fa0f25b10488ced
Bernard El-Hagin <bernard.elhagin@gmail.com>
2015-10-27 21:05:18 +0100

    Dodanie Bean'a na potrzeby obiektów CameraDTO.

--- pl.atena.aj.be.ebay/src/main/java/pl/atena/aj/be/ebay/web/CameraAction.java
+++ pl.atena.aj.be.ebay/src/main/java/pl/atena/aj/be/ebay/web/CameraAction.java
@@ -1,5 +1,16 @@
 package pl.atena.aj.be.ebay.web;
 
-public class CameraAction {
+import pl.atena.aj.be.ebay.domain.CameraDTO;
 
+public class CameraAction {
+	
+	private CameraDTO camera = new CameraDTO();
+
+	public CameraDTO getCamera() {
+		return camera;
+	}
+
+	public void setCamera(CameraDTO camera) {
+		this.camera = camera;
+	}
 }
