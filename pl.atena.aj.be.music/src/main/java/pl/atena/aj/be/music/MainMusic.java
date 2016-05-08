22529c6ce646f5d0521dd0ddb487bf75eb04540e
Bernard El-Hagin <bernard.elhagin@gmail.com>
2016-05-08 21:40:22 +0200

    Dodanie API last.fm + przykład użycia.

--- pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
+++ pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
@@ -1,17 +1,29 @@
 package pl.atena.aj.be.music;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.util.Collection;
 
-import javax.faces.model.SelectItem;
-
-import pl.atena.aj.be.music.domain.Genre;
+import de.umass.lastfm.Artist;
+import de.umass.lastfm.Chart;
+import de.umass.lastfm.User;
 
 public class MainMusic {
 
 	public static void main(String[] args) throws Exception {
+		String apiKey = "757d3b9331c25020e4b691cc8a2c1b03";
+		String user = "dev_null";
 
-		List<SelectItem> si = Genre.getAllValues();
+		Chart<Artist> chart = User.getWeeklyArtistChart(user, apiKey);
+		DateFormat format = DateFormat.getDateInstance();
 
-		System.out.println(si.toString());
+		String from = format.format(chart.getFrom());
+		String to = format.format(chart.getTo());
+
+		System.out.printf("Charts for %s for the week from %s to %s: %n", user, from, to);
+
+		Collection<Artist> artists = chart.getEntries();
+		for(Artist art : artists) {
+			System.out.println(art.getName());
+		}
 	}
 }
