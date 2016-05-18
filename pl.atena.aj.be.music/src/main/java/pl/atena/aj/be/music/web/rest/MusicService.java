eca550cde51b2cd339a01730834b943da5be2fd9
Bernard El-Hagin <bernard.elhagin@gmail.com>
2016-05-18 21:36:41 +0200

    Poprawne skonfigurowanie Swaggera.

--- pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/web/rest/MusicService.java
+++ pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/web/rest/MusicService.java
@@ -10,7 +10,7 @@ import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.jaxrs.listing.ApiListingResource;
 import io.swagger.jaxrs.listing.SwaggerSerializers;
 
-@ApplicationPath("/v1")
+@ApplicationPath("/rest")
 public class MusicService extends Application {
 
 	private Set<Class<?>> classes = new HashSet<>();
@@ -31,11 +31,12 @@ public class MusicService extends Application {
 
 	private void initSwagger() {
         BeanConfig beanConfig = new BeanConfig();
+
         beanConfig.setTitle("music_library");
         beanConfig.setVersion("1.0");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
-        beanConfig.setBasePath("/api");
+        beanConfig.setBasePath("/rest");
         beanConfig.setResourcePackage("pl.atena.aj.be.music.web.rest");
         beanConfig.setScan(true);
     }
