3e379f6251d3c4d9656fa31ddc9f0faf65daf037
Bernard El-Hagin <bernard.elhagin@gmail.com>
2016-09-05 20:29:37 +0200

    Added Vim undo files to gitignore.

--- pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
+++ pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
@@ -1,15 +1,8 @@
 package pl.atena.aj.be.music;
 
-import de.umass.lastfm.Album;
-import de.umass.lastfm.Track;
-
 public class MainMusic {
 
 	public static void main(String[] args) throws Exception {
-		String apiKey = "757d3b9331c25020e4b691cc8a2c1b03";
-
-		for(Track trck : Album.getInfo("Led Zeppelin", "II", apiKey).getTracks()) {
-			System.out.println(trck.getName());
-		}
+        System.out.print("To jest jakiś tam commit");
 	}
 }
