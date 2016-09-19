69260881d4a4b46f15c2d9af857701e2f689b87b
Bernard El-Hagin <bernard.elhagin@gmail.com>
2016-09-19 21:25:20 +0200

    Initial Spotify API.

--- pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
+++ pl.atena.aj.be.music/src/main/java/pl/atena/aj/be/music/MainMusic.java
@@ -1,8 +1,18 @@
 package pl.atena.aj.be.music;
 
+import java.io.IOException;
+
+import com.wrapper.spotify.Api;
+import com.wrapper.spotify.exceptions.WebApiException;
+import com.wrapper.spotify.methods.AlbumRequest;
+
 public class MainMusic {
 
-	public static void main(String[] args) throws Exception {
-        System.out.print("To jest jakiś tam commit");
+	public static void main(String[] args) throws IOException, WebApiException {
+		Api api = Api.DEFAULT_API;
+
+		AlbumRequest ar = api.getAlbum("7e0ij2fpWaxOEHv5fUYZjd").build();
+
+		System.out.println(ar.get().getName());
 	}
 }
